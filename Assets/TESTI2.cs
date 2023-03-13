9068ae31849b48b4068411f742e043921052d876
suppressedsheep <127407683+suppressedsheep@users.noreply.github.com>
2023-03-13 10:43:31 +0200

    Update TESTI2.cs
    paskaaa

--- Assets/TESTI2.cs
+++ Assets/TESTI2.cs
@@ -7,7 +7,7 @@ public class TESTI2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //paskotaan
     }
 
     // Update is called once per frame
