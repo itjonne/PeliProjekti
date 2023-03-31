91fdc4ed9d8f5ef3ae7a1119f52a5f4d28dc6c7f
itjonne <jovimajo@gmail.com>
2023-03-31 12:23:45 +0300

    Muutos

--- Assets/Map/Block.cs
+++ Assets/Map/Block.cs
@@ -22,20 +22,43 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
-        GetPlaneSize();
-        GenerateEnvironment();
-    }
-
-    void GenerateEnvironment()
-    {
         foreach (var obj in generatedObjects)
         {
             Destroy(obj);
         }
 
+        GetPlaneSize();
+        GenerateEnvironment();
+        GenerateHelpers();
+
+   
+    }
+
+    void GenerateEnvironment()
+    {
+
+
         foreach (var obj in environmentObjects)
         {
             int amount = (int)Random.Range(0, environmentDensity * width / 10); // T‰‰ nyt vaa testi
+            Debug.Log("AMount: " + amount);
+            for (int i = 0; i < amount; i++)
+            {
+                Vector3 randomPosition = new Vector3(Random.Range(-width / 2, width / 2), 0, Random.Range(-height / 2, height / 2));
+                Debug.Log("Random pos: " + randomPosition);
+                GameObject newObject = Instantiate(obj, transform.position + randomPosition, Quaternion.identity);
+                generatedObjects.Add(newObject);
+            }
+        }
+    }
+
+    void GenerateHelpers()
+    {
+ 
+
+        foreach (var obj in helperObjects)
+        {
+            int amount = (int)Random.Range(0, helperDensity * width / 10); // T‰‰ nyt vaa testi
             Debug.Log("AMount: " + amount);
             for (int i = 0; i < amount; i++)
             {
--- Assets/Map/MapManager.cs
+++ Assets/Map/MapManager.cs
@@ -8,7 +8,7 @@ public class MapManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateMap(9); // Tekee 9x9 mapin
+        GenerateMap(5); // Tekee 9x9 mapin
     }
 
     // Update is called once per frame
@@ -19,7 +19,7 @@ public class MapManager : MonoBehaviour
 
     public void GenerateMap(int size)
     {
-        float tileWidth = 100f;
+        float tileWidth = 50f;
         for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
