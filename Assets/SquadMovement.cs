afe62c3322bc7f4b21fd4db70aaf7a0e8ba1bebc
itjonne <jovimajo@gmail.com>
2023-04-14 12:41:09 +0300

    Muutettu movementti rawiks

--- Assets/SquadMovement.cs
+++ Assets/SquadMovement.cs
@@ -30,7 +30,6 @@ public class SquadMovement : MonoBehaviour
         _leader = GetComponent<Squad>().GetLeader().gameObject;
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
 
-        Debug.Log(targetVector);
         //Move in the direction we are aiming 
         var movementVector = MoveTowardTarget(targetVector);
         if (!rotateTowardsMouse)
