3fc3f69fe7502b657e08e808dd4ff98ccaeed395
Noiztox <isto.tohonen@gmail.com>
2023-04-26 14:15:39 +0300

    toimi pliis

--- Assets/Audio Files/AudioEnums.cs
+++ Assets/Audio Files/AudioEnums.cs
@@ -1,14 +1,10 @@
 namespace JSAM {
     public enum Sounds {
-        sfx_GrenadeExplosion,
-        sfx_GrenadePin,
         sfx_Hitmarker,
         sfx_MachineGun,
-        sfx_Pistol,
-        sfx_Reload
+        sfx_Pistol
     }
     public enum Music {
-        music_Level1,
         music_Menu
     }
 }
--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -20,7 +20,8 @@ public class CameraMovement : MonoBehaviour
         if (target)
         {
 
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 25f, 0));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 60f, 0));
+         
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
