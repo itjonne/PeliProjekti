636436dae7e384dc42af9e392e5adcb64dd50c49
Osssian <ossiheinonen_3@hotmail.com>
2023-04-26 19:05:11 +0300

    Tehty oma kranaattisysteemi

--- Assets/Scripts/GrenadeThrower.cs
+++ Assets/Scripts/GrenadeThrower.cs
@@ -8,14 +8,41 @@ public class GrenadeThrower : MonoBehaviour
     [SerializeField] private Transform Hand;
     public GameObject grenadePrefab;
     public Vector3 gravity;
+
+
+
+    Vector3 playerToMouse;
+    Vector3 mouseLocation;
+
+    float throwDistance;
+
+    GUIStyle myStyle = new GUIStyle();
+
     private void Start()
     {
+
+        myStyle.fontSize = 16;
+		myStyle.normal.textColor = Color.cyan;
+
+
         gravity = Physics.gravity;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+     
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            mouseLocation = hit.point;
+
+        }
+
+            throwDistance = Vector3.Distance(mouseLocation, transform.position);      
+
         if (Input.GetMouseButtonDown(1))
         {
             Vector3 target = Input.mousePosition;
@@ -25,8 +52,36 @@ public class GrenadeThrower : MonoBehaviour
 
     void ThrowGrenade()
     {
-        GameObject grenade = Instantiate(grenadePrefab, Hand.position, transform.rotation);
-        Rigidbody rb = grenade.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        //KRANU LENTÄÄ HIIREN KOHDALLE MUTTA EI VOI LENTÄÄ 20 UNITTIA KAUEMMAKSI
+        if (throwDistance < 20)
+        { 
+            GameObject grenade = Instantiate(grenadePrefab, Hand.position, transform.rotation);
+            Rigidbody rb = grenade.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * throwDistance * 1.15f, ForceMode.Impulse);
+            rb.AddForce(transform.up * throwDistance * 0.7f, ForceMode.Impulse);
+        }
+
+        else
+        {
+            GameObject grenade = Instantiate(grenadePrefab, Hand.position, transform.rotation);
+            Rigidbody rb = grenade.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 20 * 1.15f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 20 * 0.7f, ForceMode.Impulse);
+        }
+
+
     }
+
+
+    // HIIREN ETÄISYYDEN Debuggaukseen
+    /*
+    private void OnGUI()
+    {
+        if (gameObject.name == "SOLDIER_full")
+        {
+            GUI.Label(new Rect(300, 10, 100, 20), "distance: " + throwDistance, myStyle);
+          
+        }
+    }
+    */
 }
