b223049ba4befc75cb65aa80ad660aa5b034ea8a
stiquu <127385180+stiquu@users.noreply.github.com>
2023-04-28 12:05:53 +0300

    puukkoukko

---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    public float attackDistance = 1.5f;
+    public float damage = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Aiheuta pelaajalle vahinkoa, jos pelaaja on tarpeeksi lähellä
+            if(Vector3.Distance(transform.position, other.transform.position) <= attackDistance)
+            {
+                other.GetComponent<Character>().health -= damage;
+            }
+        }
+    }
+   
+
+}
