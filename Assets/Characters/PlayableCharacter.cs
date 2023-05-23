84420b0a0783084da2798d9903e4ff3c3e8723df
Osssian <ossiheinonen_3@hotmail.com>
2023-05-23 14:29:53 +0300

    update

--- Assets/Characters/PlayableCharacter.cs
+++ Assets/Characters/PlayableCharacter.cs
@@ -144,7 +144,8 @@ public class PlayableCharacter : Character
         gameObject.GetComponent<Animation_Soldier>()?.OnDeath(); // Tää on nyt vähän spagetti, tän ei tarviis ymmärtää mitään squadista/animaatiosta
         gameObject.GetComponent<Animation_Soldier>().enabled = false;
         gameObject.GetComponent<NavMeshAgent>().enabled = false;
-        Destroy(characterHud, 1f);
+        Destroy(grenadeHud);
+        Destroy(characterHud, 1f); //poistetaan pienellä viiveellä hudi, näkyy että palkki on tyhjä
         Destroy(gameObject, 30);
 
         // PÄIVITETTY, TEHDÄÄN SAMA RIMPSU KUN VIHULLE, TUHOTAAN KOMPONENTIT KUOLLESSA ETTÄ SAADAAN ANIMOITUA KUOLEMA JA RUUMIS PYSYY NÄKYVILLÄ 30 SEKUNTIA
